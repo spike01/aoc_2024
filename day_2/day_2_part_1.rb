1bb1af64422fc7b881eb207abfa5cbdb3bf2ab28
Spike Lindsey <spike01@gmail.com>
2024-12-03 11:35:41 -0500

    Refactor day_2_part_1

--- day_2/day_2_part_1.rb
+++ day_2/day_2_part_1.rb
@@ -1,13 +1,12 @@
 safe = 0
 
-def direction(a, b)
-  diff = b - a
-  if diff.zero?
-    0
-  elsif diff.positive?
-    1
+def sign(i)
+  if i.zero?
+    :zero
+  elsif i.positive?
+    :+
   else
-    -1
+    :-
   end
 end
 
@@ -15,22 +14,17 @@ File.readlines("input.txt").each do |line|
   parsed = line.split(" ")
     .map(&:to_i)
 
-  direction = direction(parsed[0], parsed[1])
-  is_safe = true
-
-  parsed.each.with_index do |i, idx|
-    next_item = parsed[idx + 1]
-    break unless next_item 
-
-    diff = (next_item - i).abs
-    next_direction = direction(i, next_item)
-    
-    unless next_item && next_direction == direction && 1 <= diff && diff < 4
-      is_safe = false
-    end
-    direction = next_direction
+  prev = 0
+  diffs = parsed.map do |i| 
+    ret = prev - i
+    prev = i
+    ret
   end
-  safe += 1 if is_safe
+
+  if diffs.drop(1).all? { |diff| 1 <= diff.abs && diff.abs < 4 } &&
+     diffs.drop(1).map { |diff| sign(diff) }.uniq.size <= 1
+    safe +=1 
+  end
 end
 
 p safe
