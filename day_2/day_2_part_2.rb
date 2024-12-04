080c6e8ab37579beef71c3b44bce6044cd8b2a2f
Spike Lindsey <spike01@gmail.com>
2024-12-04 14:01:05 -0500

    More ultramodern Rubying

--- day_2/day_2_part_2.rb
+++ day_2/day_2_part_2.rb
@@ -1,5 +1,3 @@
-safe = 0
-
 def sign(i)
   if i.zero?
     :zero
@@ -16,21 +14,17 @@ def same_sign?(arr) = arr.map { sign(_1) }.uniq.size <= 1
 
 def safe?(arr) = arr.all? { within_range?(_1) } && same_sign?(arr)
 
+def diffs(arr) = arr.each_cons(2).map{ _1 - _2 }
+
+safe = 0
+
 File.readlines("input.txt").each do |line|
   parsed = line.split(" ")
     .map(&:to_i)
 
   combos = parsed.combination(parsed.size - 1)
 
-  safe += 1 if combos.any? do |combo|
-    prev = combo.shift
-    diffs = combo.map do |i|
-      ret = prev - i
-      prev = i
-      ret
-    end
-    safe?(diffs)
-  end
+  safe += 1 if combos.any? { safe?(diffs(_1)) }
 end
 
 p safe
